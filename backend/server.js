f6306fd7d3a4df1271e4ed386e45c88da7f0bedf
Andrew Lee <andrewimg@gmail.com>
2023-04-19 18:09:54 -0700

    put backend in its own folder

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use('/api', apiRoutes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true })
